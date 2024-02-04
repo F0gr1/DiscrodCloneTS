0b66b3a95b25eb9f86e5a36e49c5b6ae78609dda
F0gr1 <ishigami0314@icloud.com>
2024-02-04 22:21:19 +0900

    upadate

--- src/components/chat/Chat.tsx
+++ src/components/chat/Chat.tsx
@@ -1,10 +1,33 @@
 import React from 'react'
 import './Chat.scss'
 import ChatHeader from './ChatHeader'
+import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
+import GifBoxIcon from '@mui/icons-material/GifBox';
+import CardGiftcardIcon from '@mui/icons-material/CardGiftcard';
+import EmojiEmotionsIcon from '@mui/icons-material/EmojiEmotions';
+import ChatMessage from './ChatMessage';
+
 const Chat = () => {
 	return (
 		<div className='chat'>
 			<ChatHeader/>
+			<div className="chatMessage">
+				<ChatMessage/>
+			</div>
+			<div className="chatInput">
+				<AddCircleOutlineIcon/>
+				<form>
+					<input type='text' placeholder='サトシの体重を入力'/>
+					<button type="submit" className='chatInputButton'>
+						送信
+					</button>
+				</form>
+				<div className="chatInputIcons">
+					<CardGiftcardIcon/>
+					<GifBoxIcon/>
+					<EmojiEmotionsIcon/>
+				</div>
+			</div>
 		</div>
 	)
 }
